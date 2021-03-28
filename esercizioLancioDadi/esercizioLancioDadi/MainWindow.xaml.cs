9f5da9e6cb4d0bff68ea0f7ec7e5d0a54a4921eb
SimoneGramellini <simone.gramellini.stud@ispascalcomandini.it>
2021-03-28 11:40:08 +0200

    Aggiunta logica programma e grafica

--- esercizioLancioDadi/esercizioLancioDadi/MainWindow.xaml.cs
+++ esercizioLancioDadi/esercizioLancioDadi/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace esercizioLancioDadi
 {
@@ -20,9 +21,77 @@ namespace esercizioLancioDadi
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random random;
+
         public MainWindow()
         {
             InitializeComponent();
+            random = new Random();
+            int sorteggiato = 0;
+            Sorteggio(sorteggiato);
+        }
+
+        private async void Sorteggio(int sorteggiato)
+        {
+            await Task.Run(() =>
+            {
+                while (true)
+                {
+                    sorteggiato = random.Next(1, 6);
+                    Thread.Sleep(250);
+                    this.Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        lblDado.Content = sorteggiato;
+                    }));
+
+                }
+            });
+        }
+        
+        /*
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //ExecuteLongOpSincrono();
+
+            //MessageBox.Show("Operazione sincrona completata");
+        }
+        */
+
+        private void ExecuteLongOpSincrono() //non utilizzato
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+
+        private async void ExecuteLongOpAsincrono() //non utilizzato
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Thread.Sleep(1000);
+                }
+                MessageBox.Show("Operazione asincrona completata");
+            });
+        }
+
+        private void btn_estrai_Click(object sender, RoutedEventArgs e) //non utilizzato
+        {
+            ExecuteLongOpAsincrono();
+        }
+
+        private void btnPrendiValore_Click(object sender, RoutedEventArgs e)
+        {
+            lblValoreEstratto.Content = lblDado.Content;
+            lstCronologiaValoriEstratti.Items.Add(lblValoreEstratto.Content);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            lstCronologiaValoriEstratti.Items.Clear();
+            lblValoreEstratto.Content = "";
         }
     }
 }
